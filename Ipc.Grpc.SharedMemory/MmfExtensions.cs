224285b2129d849c805a11f0a0d8726427323515
Houssam Rouidi <houssam.rouidi@gomitech.fr>
2022-06-24 13:49:42 +0200

    Introduce SharedMemory

---
+++ Ipc.Grpc.SharedMemory/MmfExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks.Sources;
+
+namespace Ipc.Grpc.SharedMemory
+{
+
+    public static class MmfExtensions
+    {
+        public static unsafe Span<byte> GetSpan(this MemoryMappedViewAccessor accessor)
+        {
+            byte* ptr = null;
+            try
+            {
+                accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref ptr);
+                Span<byte> buffer2 = new(ptr, (int)accessor.Capacity);
+                return buffer2;
+            }
+            finally
+            {
+                accessor.SafeMemoryMappedViewHandle.ReleasePointer();
+            }
+        }
+
+        public static ValueTask<int> ReadAsync(this MemoryMappedViewAccessor accessor, long position, Memory<byte> buffer, CancellationToken token = default)
+        {
+            if (MemoryMarshal.TryGetArray(buffer, out ArraySegment<byte> segment) == false)
+                throw new NotSupportedException("Array-based buffer required");
+            int count = accessor.ReadArray(position, segment.Array!, segment.Offset, segment.Count);
+            return new ValueTask<int>(count);
+        }
+    }
+}
